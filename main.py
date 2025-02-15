c70a5e372e519f0904992b3a5b5cca38752c9a09
bycarlosgamez <hardpx@gmail.com>
2025-02-15 16:23:25 -0600

    finished

---
+++ main.py
@@ -0,0 +1,55 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_chars = get_num_chars(text)
+    sorted_dic = get_sorted_dic(num_chars)
+    dic_list = get_dic_list(sorted_dic)
+    report = get_report(dic_list, book_path, num_words)
+    # print(f"{num_words}")
+    # print(num_chars)
+    # print(dic_list)
+    return report
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+
+def get_num_chars(text):
+    lowerText = text.lower()
+    chars = {}
+    for char in lowerText:
+        if char in chars:
+            chars[char] += 1;
+        else:
+            chars[char] = 1;
+    return chars
+
+
+def get_sorted_dic(num_chars):
+    sorted_dic = dict(sorted(num_chars.items(), reverse=True, key=lambda item: item[1]))
+    return sorted_dic
+
+def get_dic_list(sorted_dic):
+     letters = []
+     for key, value in sorted_dic.items():
+        if key.isalpha() == True:
+            letters.append({"character": key, "num": value})
+     return letters
+
+def get_report(dic_list, book_path, num_words):
+    print(f"--- Begin report of {book_path} --- {num_words} words found in the document")
+    for letter in dic_list:
+        print(f"The '{letter["character"]}' character was found {letter["num"]} times")
+    print("--- End report ---")
+
+
+
+main()
